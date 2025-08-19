1fcecb886d2f87b8a064b17ecb01704def533258
Wael Shaheen <3201818904@alexu.edu.eg>
2025-08-20 01:35:00 +0300

    fix vite base path for github pages

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/wael-portfolio/',   // 👈 ده مهم عشان GitHub Pages
 })
